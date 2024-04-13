11419324db561bd308e262cf3574474cc78f78e3
GusContini <gcontini.torres@gmail.com>
2024-04-13 12:35:29 -0300

    added port to main script

--- main.py
+++ main.py
@@ -99,3 +99,7 @@ async def predict_income(input_data: InputData):
         return {'predictions': str(predictions) + pred_label}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
